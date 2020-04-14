14e3f8437f7e800aa5f2976c5040b9fc8d7230c6
Michael wu <mwuf15@gmail.com>
2020-04-14 15:47:37 -0700

    completed delete using unlink

--- datastore/index.js
+++ datastore/index.js
@@ -77,14 +77,9 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  fs.unlink(path.join(exports.dataDir, `${id}.txt`), (err) => {
+    callback(err);
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
